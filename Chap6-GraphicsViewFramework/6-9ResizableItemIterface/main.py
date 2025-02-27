ad414a440b939c9ccf4c190280d76d43455368ad
EricoDeMecha <ford.koech.eric@gmail.com>
2025-02-27 10:55:56 +0300

    (ch6-pyside6-update): ported ch6/6-9

---
+++ Chap6-GraphicsViewFramework/6-9ResizableItemIterface/main.py
@@ -0,0 +1,19 @@
+import sys
+from PySide6.QtWidgets import QApplication
+from widget import Widget
+
+# Import the resources
+import resource_rc
+
+def main():
+    """
+    Application entry point.
+    """
+    app = QApplication(sys.argv)
+    window = Widget()
+    window.show()
+    
+    return app.exec()
+
+if __name__ == "__main__":
+    sys.exit(main())

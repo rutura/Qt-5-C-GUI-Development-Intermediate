2e9203baa714de5691042df167cbb5a5b4cdb34a
EricoDeMecha <ford.koech.eric@gmail.com>
2025-02-27 16:13:37 +0300

    (ch7-pyside6-update): ported ch7/7-1

---
+++ Chap7-Miscellaneous/7-1PainterAppBrushProperties/main.py
@@ -0,0 +1,13 @@
+import sys
+from PySide6.QtWidgets import QApplication
+from mainwindow import MainWindow
+
+def main():
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    
+    return app.exec()
+
+if __name__ == "__main__":
+    sys.exit(main())
